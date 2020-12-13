41b6d06e98b4f8e7f9baf8895e3f0248c8d10edb
theankushrai <iamankushrai@gmail.com>
2020-12-13 14:52:06 +0530

    1. on draw implemented

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -16,11 +16,16 @@ class MyCanvasView(context: Context): View(context) {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        // removing old extrabitmap and canvas instances before creating new ones
+        // removing old extra bitmap and canvas instances before creating new ones
         if(::extraBitmap.isInitialized) extraBitmap.recycle()
         //bitmap and canvas are created every time user changes the canvas
         extraBitmap= Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
         extraCanvas=Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(extraBitmap,0f,0f,null)
     }
 }
