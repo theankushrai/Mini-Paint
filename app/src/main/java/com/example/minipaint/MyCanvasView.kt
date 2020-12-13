064fb2f2be1a41ce011e38dd1a71003e78409d09
theankushrai <iamankushrai@gmail.com>
2020-12-13 13:43:29 +0530

    1. cache set up in form of extra bitmap and extra canvas

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,9 +1,26 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context): View(context) {
 
+    private lateinit var extraBitmap: Bitmap
+    private lateinit var extraCanvas:Canvas
 
+    private val backgroundColor =ResourcesCompat.getColor(resources,R.color.colorBackground,null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        // removing old extrabitmap and canvas instances before creating new ones
+        if(::extraBitmap.isInitialized) extraBitmap.recycle()
+        //bitmap and canvas are created every time user changes the canvas
+        extraBitmap= Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        extraCanvas=Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
